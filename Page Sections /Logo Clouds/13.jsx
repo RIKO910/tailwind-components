542e3cf1b92fef89989e36b88c03fa895cc7a29d
RIKO910 <tarikulislamriko910@gmail.com>
2024-10-19 19:08:02 +0600

    Logo Clouds

---
+++ Page Sections /Logo Clouds/13.jsx
@@ -0,0 +1,44 @@
+export default function Example() {
+    return (
+        <div className="bg-indigo-700">
+            <div className="mx-auto max-w-7xl px-6 py-16 sm:py-20 lg:px-8">
+                <h2 className="text-3xl font-bold tracking-tight text-white">Trusted by the world’s most innovative teams</h2>
+                <div className="mt-8 flow-root lg:mt-10">
+                    <div className="-ml-8 -mt-4 flex flex-wrap justify-between lg:-ml-4">
+                        <div className="ml-8 mt-4 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+                            <img alt="Tuple" src="https://tailwindui.com/plus/img/logos/tuple-logo-indigo-300.svg" className="h-12" />
+                        </div>
+                        <div className="ml-8 mt-4 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+                            <img
+                                alt="Mirage"
+                                src="https://tailwindui.com/plus/img/logos/mirage-logo-indigo-300.svg"
+                                className="h-12"
+                            />
+                        </div>
+                        <div className="ml-8 mt-4 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+                            <img
+                                alt="StaticKit"
+                                src="https://tailwindui.com/plus/img/logos/statickit-logo-indigo-300.svg"
+                                className="h-12"
+                            />
+                        </div>
+                        <div className="ml-8 mt-4 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+                            <img
+                                alt="Transistor"
+                                src="https://tailwindui.com/plus/img/logos/transistor-logo-indigo-300.svg"
+                                className="h-12"
+                            />
+                        </div>
+                        <div className="ml-8 mt-4 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+                            <img
+                                alt="Workcation"
+                                src="https://tailwindui.com/plus/img/logos/workcation-logo-indigo-300.svg"
+                                className="h-12"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ Page Sections /Logo Clouds/14.jsx
@@ -0,0 +1,38 @@
+export default function Example() {
+    return (
+        <div className="bg-indigo-200 bg-opacity-25">
+            <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
+                <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                    <h2 className="mx-auto max-w-md text-pretty text-center text-3xl font-bold tracking-tight text-indigo-900 lg:max-w-xl lg:text-left">
+                        Trusted by the world’s most innovative teams
+                    </h2>
+                    <div className="mt-8 flow-root self-center lg:mt-0">
+                        <div className="-ml-8 -mt-4 flex flex-wrap justify-between lg:-ml-4">
+                            <div className="ml-8 mt-4 flex flex-shrink-0 flex-grow justify-center lg:ml-4 lg:flex-grow-0">
+                                <img
+                                    alt="Workcation"
+                                    src="https://tailwindui.com/plus/img/logos/workcation-logo-indigo-900.svg"
+                                    className="h-12"
+                                />
+                            </div>
+                            <div className="ml-8 mt-4 flex flex-shrink-0 flex-grow justify-center lg:ml-4 lg:flex-grow-0">
+                                <img
+                                    alt="Tuple"
+                                    src="https://tailwindui.com/plus/img/logos/tuple-logo-indigo-900.svg"
+                                    className="h-12"
+                                />
+                            </div>
+                            <div className="ml-8 mt-4 flex flex-shrink-0 flex-grow justify-center lg:ml-4 lg:flex-grow-0">
+                                <img
+                                    alt="Level"
+                                    src="https://tailwindui.com/plus/img/logos/level-logo-indigo-900.svg"
+                                    className="h-12"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
